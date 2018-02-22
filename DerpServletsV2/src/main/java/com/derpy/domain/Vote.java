ae7035d26040f449c1fd7235217caf245cfd458a
jaboyles <jacobboyles0115@gmail.com>
2018-02-22 13:24:42 -0500

    added some s=annotations i forgot

--- DerpServletsV2/src/main/java/com/derpy/domain/Vote.java
+++ DerpServletsV2/src/main/java/com/derpy/domain/Vote.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -11,7 +12,10 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "VOTE")
 public class Vote implements Serializable{
 
 	/**
